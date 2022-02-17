63df327bb9b3a388039fe16aad1af8b38012ecb0
yunyoung1819 <yunyoung1819@hanmail.net>
2022-02-18 01:59:45 +0900

    [ADD] 스프링 시큐리티 설정 파일 추가

---
+++ src/main/java/com/dnd/gratz/controller/user/UserController.java
@@ -0,0 +1,11 @@
+package com.dnd.gratz.controller.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class UserController {
+
+    
+}
---
+++ src/main/java/com/dnd/gratz/security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.dnd.gratz.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final GratzOAuthService gratzOAuthService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/", "/gratz/**").permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login()
+                        .userInfoEndpoint()
+                .userService(gratzOAuthService);
+    }
+}
